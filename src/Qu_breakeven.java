66525d9c08dbee808a85d3936c983d4a0614fccd
AkhilPro7 <akhileshbaboolull@gmail.com>
2019-07-30 09:35:05 +0400

    Added comments

--- src/Qu_breakeven.java
+++ src/Qu_breakeven.java
@@ -13,7 +13,8 @@ public class Qu_breakeven {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner myInput = new Scanner(System.in);
+		Scanner myInput = new Scanner(System.in); //identifier for user input
+		
 		
 		double totalcosts;
 		double totalrevenue;
@@ -23,10 +24,10 @@ public class Qu_breakeven {
 		do {
 			
 			
-			System.out.print("Enter Total Costs: ");
+			System.out.print("Enter Total Costs: "); //input total costs
 			totalcosts = myInput.nextDouble();
 			
-			if(totalcosts <= 0) {
+			if(totalcosts <= 0) { //validating total costs
 				System.out.println("Invalid value for Total Costs entered! Please Re-Enter!");
 			}
 			
@@ -36,10 +37,10 @@ public class Qu_breakeven {
 		do {
 			
 			
-			System.out.print("Enter Total Revenue: ");
+			System.out.print("Enter Total Revenue: "); //input total revenue
 			totalrevenue = myInput.nextDouble();
 			
-			if(totalrevenue <= 0) {
+			if(totalrevenue <= 0) { //validating total revenue
 				System.out.println("Invalid value for Total Revenue entered! Please Re-Enter!");
 			}
 			
@@ -47,17 +48,17 @@ public class Qu_breakeven {
 		}while(totalrevenue <= 0);
 		
 		
-		if(totalcosts == totalrevenue) {
+		if(totalcosts == totalrevenue) { //checking for break even
 			
 			System.out.println("Break Even");	
 		}
-		else if(totalcosts < totalrevenue) {
+		else if(totalcosts < totalrevenue) { //checking for profit
 			
 			System.out.print("Profit");
 			
 		}
 		else {
-			System.out.print("Loss");
+			System.out.print("Loss"); //output loss
 		}
 		
 		
